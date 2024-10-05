1824ccaaec6b2430ddbc5f9f5e2a4b2098539f5d
boilmyplate <john.maranan@proton.me>
2024-10-05 22:19:38 +1300

    make use of components instead of redirect to page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
+"use client";
+
+import React from "react";
 import Login from "./(component)/Login/login";
+import Navbar from "./(component)/NavBar/navbar";
+import HomePage from "./(component)/Home/home";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./(component)/Firebase/firebase";
+
 export default function Home() {
-	return (
-		<>
-			<Login />
-		</>
-	);
+    const [user] = useAuthState(auth);
+
+    if (!user) {
+        return <Login />;
+    } else {
+        return (
+            <>
+                <Navbar />
+                <HomePage />
+            </>
+        );
+    }
 }
