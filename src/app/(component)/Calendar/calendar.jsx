19bdbe1e19864add18af86ab5aff0e149c76a81a
boilmyplate <john.maranan@proton.me>
2024-10-18 21:17:51 +1300

    fix calendar

--- src/app/(component)/Calendar/calendar.jsx
+++ src/app/(component)/Calendar/calendar.jsx
@@ -17,24 +17,6 @@ const Calendar = () => {
     const eventsRef = useRef(null);
     const user = auth.currentUser.uid;
 
-    // useEffect(() => {
-    //     const unsubscribe = onAuthStateChanged(auth, async (user) => {
-    //         try {
-    //             setuser(user.uid);
-    //             const eventsRef = collection(db, "calendar", user, "events"); // Reference to the user's document
-    //             const snapshot = await getDocs(eventsRef); // Fetch the user's document
-    //             const fetchedEvents = snapshot.docs.map(doc => doc.data());
-    //             console.log(fetchedEvents);
-    //             setEvents(fetchedEvents); // Set the events state with fetched events
-    //         } catch (error) {
-    //             console.error("Fetch Error: ", error);
-    //         }
-    //     });
-
-    //     // Clean up listener when component unmounts
-    //     return () => unsubscribe();
-    // }, []);
-
     useEffect(() => {
         const fetchData = async () => {
             eventsRef.current = await fetchEvents(user);
@@ -54,63 +36,71 @@ const Calendar = () => {
     const handleSubmit = async e => {
         e.preventDefault(); // Prevent default form submission
         if (eventTitle && startTime && endTime && user) {
-            await addEvent(user, eventTitle, startTime, endTime);
+            eventsRef.current = await addEvent(
+                user,
+                eventTitle,
+                startTime,
+                endTime
+            );
+            setEvents(eventsRef.current);
+
             setEventTitle(""); // Clear the input fields
             setStartTime(""); // Clear the start time
             setEndTime(""); // Clear the end time
             setSelectedDate(null); // Clear the selected date
-            console.log("inside handleSubmit(): ", events);
         }
     };
 
     // const [removeEvent, setRemoveEvent] = useState([]);
 
     return (
-        <div className={styles.calendarContainer}>
-            <div className={styles.form}>
-                {/* Form for Adding New Events */}
-                <form onSubmit={handleSubmit} className={styles.eventForm}>
-                    <input
-                        type="text"
-                        name="title"
-                        placeholder="Event Title"
-                        value={eventTitle}
-                        onChange={e => setEventTitle(e.target.value)}
-                        required
-                    />
-                    <input
-                        type="datetime-local"
-                        name="start"
-                        placeholder="Start Time"
-                        value={startTime}
-                        onChange={e => setStartTime(e.target.value)}
-                        required
-                    />
-                    <input
-                        type="datetime-local"
-                        name="end"
-                        placeholder="End Time"
-                        value={endTime}
-                        onChange={e => setEndTime(e.target.value)}
-                        required
-                    />
-                    <button type="submit">Add Event</button>
-                </form>
-            </div>
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <div className={styles.form}>
+                    {/* Form for Adding New Events */}
+                    <form onSubmit={handleSubmit} className={styles.eventForm}>
+                        <input
+                            type="text"
+                            name="title"
+                            placeholder="Event Title"
+                            value={eventTitle}
+                            onChange={e => setEventTitle(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="datetime-local"
+                            name="start"
+                            placeholder="Start Time"
+                            value={startTime}
+                            onChange={e => setStartTime(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="datetime-local"
+                            name="end"
+                            placeholder="End Time"
+                            value={endTime}
+                            onChange={e => setEndTime(e.target.value)}
+                            required
+                        />
+                        <button type="submit">Add Event</button>
+                    </form>
+                </div>
 
-            <FullCalendar
-                key={events.length}
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-                initialView={"dayGridMonth"}
-                headerToolbar={{
-                    start: "today prev,next",
-                    center: "title",
-                    end: "dayGridMonth,timeGridWeek,timeGridDay"
-                }}
-                height={"90vh"}
-                events={events} // Use the events state
-                dateClick={handleDateClick} // Handle date click
-            />
+                <FullCalendar
+                    key={events.length}
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                    initialView={"dayGridMonth"}
+                    headerToolbar={{
+                        start: "today prev,next",
+                        center: "title",
+                        end: "dayGridMonth,timeGridWeek,timeGridDay"
+                    }}
+                    height={"70vh"}
+                    events={events} // Use the events state
+                    dateClick={handleDateClick} // Handle date click
+                />
+            </div>
         </div>
     );
 };
--- src/app/(component)/Firebase/firestore/calendarDB.js
+++ src/app/(component)/Firebase/firestore/calendarDB.js
@@ -4,12 +4,14 @@ import { db } from "../firebase";
 export const fetchEvents = async uid => {
     const eventsRef = collection(db, "users", uid, "calendar");
     const snapshot = await getDocs(eventsRef);
-    const fetchedEvents = snapshot.docs.map(doc => ({ 
-        end: doc.end,
-        start: doc.start,
-        title: doc.title, 
-    }));
+    const fetchedEvents = snapshot.docs.map(doc => doc.data());
 
+    if (fetchedEvents.length < 1) {
+        console.error("No document");
+        return [];
+    }
+
+    // console.log("fetchEvents: ", fetchEvents);
     return fetchedEvents;
 };
 
@@ -21,6 +23,10 @@ export const addEvent = async (uid, eventTitle, startTime, endTime) => {
             start: startTime,
             end: endTime
         }); // Add new event to Firestore
+        console.log("Successfully added event");
+
+        const events = await fetchEvents(uid); // Fetch events after adding
+        return events;
     } catch (e) {
         console.error("Error adding event: ", e);
     }
