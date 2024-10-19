a8e747a818ec7573f9604de594d356e75a306957
boilmyplate <john.maranan@proton.me>
2024-10-19 14:18:25 +1300

    start of implementing unit tests

---
+++ tests/components/Home.test.jsx
@@ -0,0 +1,8 @@
+import Home from "@/app/(component)/Home/Home";
+import { render } from "@testing-library/react";
+
+describe("Home", () => {
+    it("should render the login page if the user is not logged in, otherwise render the dashboard", () => {
+
+    });
+});
